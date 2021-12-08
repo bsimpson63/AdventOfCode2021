147b00114359bb69d6c19589c9b9be145b73ab9e
Brian Simpson <bsimpson63@gmail.com>
2021-12-07 19:51:45 -0800

    day 7 solutions

---
+++ 07/part1/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func calcFuel(crabs []int, pos int) int {
+	f := 0
+	for _, p := range crabs {
+		if p > pos {
+			f += (p - pos)
+		} else if p < pos {
+			f += (pos - p)
+		}
+	}
+	return f
+}
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		fmt.Println("Can't read input")
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	crabs := make([]int, 0)
+	s := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ",")
+		for _, p := range parts {
+			f, err := strconv.Atoi(p)
+			if err != nil {
+				fmt.Println("bad input")
+				return
+			}
+			crabs = append(crabs, f)
+			s += f
+		}
+	}
+	sort.Ints(crabs)
+
+	minPos := 0
+	minFuel := calcFuel(crabs, minPos)
+	for pos := crabs[0]; pos <= crabs[len(crabs)-1]; pos++ {
+		fuel := calcFuel(crabs, pos)
+		if fuel < minFuel {
+			minFuel = fuel
+			minPos = pos
+		}
+	}
+	fmt.Println("min fuel at", minPos, "is", minFuel)
+
+}
